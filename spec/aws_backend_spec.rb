080c4486f9ed6e991b9285fd67ed921348ecd507
Mathias Lafeldt <mathias.lafeldt@gmail.com>
2013-12-12 14:09:04 +0100

    Test Aws_backend#lookup in dedicated block

--- spec/aws_backend_spec.rb
+++ spec/aws_backend_spec.rb
@@ -4,29 +4,31 @@ class Hiera
   module Backend
     describe Aws_backend do
       let(:backend) { Aws_backend.new }
-      let(:lookup_key) { "some_key" }
-      let(:lookup_scope) { { "foo" => "bar" } }
-      let(:lookup_params) { [lookup_key, lookup_scope, "", :priority] }
 
       before do
         Hiera.stub(:debug)
       end
 
-      it "returns nil if hierarchy is empty" do
-        Backend.stub(:datasources)
-        expect(backend.lookup(*lookup_params)).to be_nil
-      end
+      describe "#lookup" do
+        let(:key) { "some_key" }
+        let(:scope) { { "foo" => "bar" } }
+        let(:params) { [key, scope, "", :priority] }
 
-      it "returns nil if service is unknown" do
-        Backend.stub(:datasources).and_yield "aws/unknown_service"
-        expect(backend.lookup(*lookup_params)).to be_nil
-      end
+        it "returns nil if hierarchy is empty" do
+          Backend.stub(:datasources)
+          expect(backend.lookup(*params)).to be_nil
+        end
 
-      it "properly forwards lookup to ElastiCache service" do
-        Backend.stub(:datasources).and_yield "aws/elasticache"
-        Aws::ElastiCache.any_instance.should_receive(:lookup).
-          with(lookup_key, lookup_scope)
-        backend.lookup(*lookup_params)
+        it "returns nil if service is unknown" do
+          Backend.stub(:datasources).and_yield "aws/unknown_service"
+          expect(backend.lookup(*params)).to be_nil
+        end
+
+        it "properly forwards lookup to ElastiCache service" do
+          Backend.stub(:datasources).and_yield "aws/elasticache"
+          Aws::ElastiCache.any_instance.should_receive(:lookup).with(key, scope)
+          backend.lookup(*params)
+        end
       end
     end
   end
