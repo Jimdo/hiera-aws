531ae16a9eed1800512c9c67b7d80699d9154ee4
Mathias Lafeldt <mathias.lafeldt@gmail.com>
2014-02-18 11:36:20 +0100

    Add skeleton for new RDS service

---
+++ lib/hiera/backend/aws/rds.rb
@@ -0,0 +1,11 @@
+require "hiera/backend/aws/base"
+
+class Hiera
+  module Backend
+    module Aws
+      # Implementation of Hiera keys for aws/rds
+      class RDS < Base
+      end
+    end
+  end
+end
---
+++ spec/aws_rds_spec.rb
@@ -0,0 +1,8 @@
+require "hiera/backend/aws/rds"
+
+class Hiera
+  module Backend # rubocop:disable Documentation
+    describe Aws::RDS do
+    end
+  end
+end
