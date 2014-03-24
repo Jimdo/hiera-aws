c5c3344669d27950f306fc937e7fd9d3fa8a0c80
Soenke Ruempler <soenke@jimdo.com>
2014-03-24 12:01:41 +0100

    Remove logic from fixture data

--- spec/aws_elasticache_spec.rb
+++ spec/aws_elasticache_spec.rb
@@ -3,33 +3,29 @@ require "hiera/backend/aws/elasticache"
 class Hiera
   module Backend # rubocop:disable Documentation
     describe Aws::ElastiCache do
-      let(:ec_redis_client) do
-        double(
-          :describe_cache_clusters => {
-            :cache_clusters => [{
-              :cache_nodes => [
-                { :endpoint => { :address => "1.1.1.1", :port => 1234 } },
-                { :endpoint => { :address => "2.2.2.2", :port => 1234 } }
+      let(:redis_cache_clusters) do
+        {
+          :cache_clusters => [{
+            :cache_nodes => [
+              { :endpoint => { :address => "1.1.1.1", :port => 1234 } },
+              { :endpoint => { :address => "2.2.2.2", :port => 1234 } }
 
-              ],
-              :engine => "redis"
-            }]
-          }
-        )
+            ],
+            :engine => "redis"
+          }]
+        }
       end
-      let(:ec_memcached_client) do
-        double(
-          :describe_cache_clusters => {
-            :cache_clusters => [{
-              :cache_nodes => [
-                { :endpoint => { :address => "3.3.3.3", :port => 5678 } },
-                { :endpoint => { :address => "4.4.4.4", :port => 5678 } }
+      let(:memcached_cache_clusters) do
+        {
+          :cache_clusters => [{
+            :cache_nodes => [
+              { :endpoint => { :address => "3.3.3.3", :port => 5678 } },
+              { :endpoint => { :address => "4.4.4.4", :port => 5678 } }
 
-              ],
-              :engine => "memcached"
-            }]
-          }
-        )
+            ],
+            :engine => "memcached"
+          }]
+        }
       end
       let(:ec2_client) do
         double(
@@ -71,7 +67,11 @@ class Hiera
         it "returns all Redis cluster nodes for CloudFormation stack of EC2 instance" do
           scope = { "ec2_instance_id" => "some-ec2-instance-id" }
           elasticache = Aws::ElastiCache.new scope
+
+          ec_redis_client = double
+          allow(ec_redis_client).to receive(:describe_cache_clusters).and_return(redis_cache_clusters)
           AWS::ElastiCache::Client.stub(:new => ec_redis_client)
+
           expect(elasticache.redis_cluster_nodes_for_cfn_stack).to eq [
             {
               "endpoint" => { "address" => "1.1.1.1", "port" => 1234 }
@@ -94,7 +94,11 @@ class Hiera
         it "returns all Memcached cluster nodes for CloudFormation stack of EC2 instance" do
           scope = { "ec2_instance_id" => "some-ec2-instance-id" }
           elasticache = Aws::ElastiCache.new scope
+
+          ec_memcached_client = double
+          allow(ec_memcached_client).to receive(:describe_cache_clusters).and_return(memcached_cache_clusters)
           AWS::ElastiCache::Client.stub(:new => ec_memcached_client)
+
           expect(elasticache.memcached_cluster_nodes_for_cfn_stack).to eq [
             {
               "endpoint" => { "address" => "3.3.3.3", "port" => 5678 }
