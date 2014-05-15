08be97607e06bdfc25dfdde33c8f3eca8b6a402d
Deniz Adrian <deniz@adrianer.de>
2014-05-15 13:58:56 +0200

    add failing test for multiple clusters in one replica
    cleanup test stubs and remove ordering dependencies

--- spec/aws_elasticache_spec.rb
+++ spec/aws_elasticache_spec.rb
@@ -6,6 +6,9 @@ class Hiera
   module Backend # rubocop:disable Documentation
     describe Aws::ElastiCache do
 
+      let(:time_earlier) { Time.new }
+      let(:time_later) { time_earlier + 1 }
+
       let(:ec2_client) do
         double(
           :instances => {
@@ -21,21 +24,23 @@ class Hiera
           :stacks => {
             "some-stack-name" => double(
               :resources => [
-                double(
-                  :resource_type => "AWS::ElastiCache::CacheCluster",
-                  :physical_resource_id => "some-redis-cluster-id"
-                ),
+                # these are listed in reverse/random order to proove that
+                # tests don't rely on ordering.
                 double(
                   :resource_type => "AWS::ElastiCache::CacheCluster",
                   :physical_resource_id => "another-redis-cluster-id"
                 ),
                 double(
                   :resource_type => "AWS::ElastiCache::CacheCluster",
-                  :physical_resource_id => "some-memcache-cluster-id"
+                  :physical_resource_id => "some-redis-cluster-id"
                 ),
                 double(
                   :resource_type => "AWS::ElastiCache::CacheCluster",
                   :physical_resource_id => "another-memcache-cluster-id"
+                ),
+                double(
+                  :resource_type => "AWS::ElastiCache::CacheCluster",
+                  :physical_resource_id => "some-memcache-cluster-id"
                 ),
               ]
             )
@@ -58,6 +63,7 @@ class Hiera
           :cache_nodes          => [some_cache_node, another_cache_node],
           :engine               => "redis",
           :cache_cluster_status => "available",
+          :cache_cluster_create_time => time_earlier,
         }
       end
 
@@ -66,6 +72,7 @@ class Hiera
           :cache_nodes          => [third_cache_node, fourth_cache_node],
           :engine               => "redis",
           :cache_cluster_status => "available",
+          :cache_cluster_create_time => time_later,
         }
       end
 
@@ -103,15 +110,29 @@ class Hiera
         }
       end
 
-      let(:replication_groups) do
+      let(:some_replication_group) do
         {
-          :replication_groups => [{
-            :replication_group_id => "some-replication-group-id",
-            :node_groups => [{
-              :primary_endpoint => { :address => "some.replication.group.primary.endpoint", :port => 1234 },
-            }],
-            :status => "available",
-          }]
+          :replication_group_id => "some-replication-group-id",
+          :node_groups => [{
+            :primary_endpoint => { :address => "some.replication.group.primary.endpoint", :port => 12_345 },
+          }],
+          :status => "available",
+        }
+      end
+
+      let(:another_replication_group) do
+        {
+          :replication_group_id => "another-replication-group-id",
+          :node_groups => [{
+            :primary_endpoint => { :address => "another.replication.group.primary.endpoint", :port => 23_456 },
+          }],
+          :status => "available",
+        }
+      end
+
+      let(:all_replication_groups) do
+        {
+          :replication_groups => [some_replication_group, another_replication_group]
         }
       end
 
@@ -122,25 +143,47 @@ class Hiera
 
         allow(@client).to receive(:describe_cache_clusters).and_return(all_cache_clusters)
 
-        allow(@client).to receive(:describe_cache_clusters).with(
-                                                                   :cache_cluster_id => "some-redis-cluster-id",
-                                                                   :show_cache_node_info => true
-        ).and_return(:cache_clusters => [some_redis_cache_cluster])
-        allow(@client).to receive(:describe_cache_clusters).with(
-                                                                   :cache_cluster_id => "another-redis-cluster-id",
-                                                                   :show_cache_node_info => true
-        ).and_return(:cache_clusters => [another_redis_cache_cluster])
+        allow(@client).to receive(:describe_cache_clusters)
+          .with(
+            :cache_cluster_id => "some-redis-cluster-id",
+            :show_cache_node_info => true
+          )
+          .and_return(:cache_clusters => [some_redis_cache_cluster])
 
-        allow(@client).to receive(:describe_cache_clusters).with(
-                                                                   :cache_cluster_id => "some-memcache-cluster-id",
-                                                                   :show_cache_node_info => true
-        ).and_return(:cache_clusters => [some_memcache_cache_cluster])
-        allow(@client).to receive(:describe_cache_clusters).with(
-                                                                   :cache_cluster_id => "another-memcache-cluster-id",
-                                                                   :show_cache_node_info => true
-        ).and_return(:cache_clusters => [another_memcache_cache_cluster])
+        allow(@client).to receive(:describe_cache_clusters)
+          .with(
+            :cache_cluster_id => "another-redis-cluster-id",
+            :show_cache_node_info => true
+          )
+          .and_return(:cache_clusters => [another_redis_cache_cluster])
 
-        allow(@client).to receive(:describe_replication_groups).with(:replication_group_id => "some-replication-group-id").and_return(replication_groups)
+        allow(@client).to receive(:describe_cache_clusters)
+          .with(
+            :cache_cluster_id => "some-memcache-cluster-id",
+            :show_cache_node_info => true
+          )
+          .and_return(:cache_clusters => [some_memcache_cache_cluster])
+
+        allow(@client).to receive(:describe_cache_clusters)
+          .with(
+            :cache_cluster_id => "another-memcache-cluster-id",
+            :show_cache_node_info => true
+          )
+          .and_return(:cache_clusters => [another_memcache_cache_cluster])
+
+        allow(@client).to receive(:describe_replication_groups)
+          .with(
+            :replication_group_id => "some-replication-group-id"
+          )
+          .and_return(:replication_groups => [some_replication_group])
+
+        allow(@client).to receive(:describe_replication_groups)
+          .with(
+            :replication_group_id => "another-replication-group-id"
+          )
+          .and_return(:replication_groups => [another_replication_group])
+
+        allow(@client).to receive(:describe_replication_groups).with({}).and_return(all_replication_groups)
       end
 
       describe "#redis_cluster_nodes_for_cfn_stack" do
@@ -154,7 +197,7 @@ class Hiera
 
         it "returns all Redis cluster nodes for CloudFormation stack of EC2 instance" do
           AWS::ElastiCache::Client.stub(:new => @client)
-          expect(elasticache.redis_cluster_nodes_for_cfn_stack).to eq [
+          elasticache.redis_cluster_nodes_for_cfn_stack.should =~ [
             { "endpoint" => { "address" => "1.1.1.1", "port" => 1111 } },
             { "endpoint" => { "address" => "2.2.2.2", "port" => 2222 } },
             { "endpoint" => { "address" => "3.3.3.3", "port" => 3333 } },
@@ -165,11 +208,11 @@ class Hiera
 
       describe "#redis_cluster_replica_groups_for_cfn_stack" do
 
-        context "multiple defined cache clusters are in the same replica group" do
+        let(:some_redis_cache_cluster) do
+          super().merge :replication_group_id => "some-replication-group-id"
+        end
 
-          let(:some_redis_cache_cluster) do
-            super().merge :replication_group_id => "some-replication-group-id"
-          end
+        context "multiple defined cache clusters are in the same replica group" do
 
           let(:another_redis_cache_cluster) do
             super().merge :replication_group_id => "some-replication-group-id"
@@ -177,78 +220,56 @@ class Hiera
 
           it "returns a deduplicated list of Redis replica groups" do
             AWS::ElastiCache::Client.stub(:new => @client)
+            actual = elasticache.redis_cluster_replica_groups_for_cfn_stack
+            expect(actual.length).to be 1
+            expect(actual.first).to include(
+                "replication_group_id" => "some-replication-group-id",
+                "primary_endpoint"     => { "address" => "some.replication.group.primary.endpoint", "port" => 12_345 }
+            )
+          end
+        end
 
-            expect(elasticache.redis_cluster_replica_groups_for_cfn_stack).to eq [
+        context "multiple defined cache clusters with different creation times are in the same replica group" do
+
+          let(:another_redis_cache_cluster) do
+            super().merge :replication_group_id => "some-replication-group-id"
+          end
+
+          it "returns the most current cache cluster creation timestamp for the whole replication group in order to know which replica group is the most current" do
+            AWS::ElastiCache::Client.stub(:new => @client)
+
+            actual = elasticache.redis_cluster_replica_groups_for_cfn_stack
+
+            actual.should eq [
               {
                 "replication_group_id" => "some-replication-group-id",
-                "primary_endpoint"     => { "address" => "some.replication.group.primary.endpoint", "port" => 1234 }
-              },
+                "primary_endpoint"     => { "address" => "some.replication.group.primary.endpoint", "port" => 12_345 },
+                "latest_cache_cluster_create_time" => time_later.strftime("%s"),
+              }
             ]
           end
         end
 
         context "two replica groups with one cache cluster each, one cache cluster being newer than the other" do
-          let(:time_earlier) { Time.new() }
-          let(:time_later) { time_earlier + 1 }
-          let(:cache_clusters) do
-            {
-              :cache_clusters => [
-                {
-                  :cache_nodes          => [some_cache_node],
-                  :replication_group_id => "some-group-id",
-                  :engine               => "redis",
-                  :cache_cluster_status => "available",
-                  :cache_cluster_create_time => time_earlier,
-                },
-                {
-                  :cache_nodes          => [another_cache_node],
-                  :replication_group_id => "another-group-id",
-                  :engine               => "redis",
-                  :cache_cluster_status => "available",
-                  :cache_cluster_create_time => time_later,
-                },
-              ]
-            }
-          end
 
-          let(:replication_groups_some_cluster) do
-            {
-              :replication_groups => [{
-                :replication_group_id => "some-group-id",
-                :node_groups => [{
-                  :primary_endpoint => { :address => "some.replication.group.primary.endpoint", :port => 1234 },
-                }],
-                :status => "available" }]
-            }
-          end
-
-          let(:replication_groups_another_cluster) do
-            {
-              :replication_groups => [{
-                :replication_group_id => "another-group-id",
-                :node_groups => [{
-                  :primary_endpoint => { :address => "another.replication.group.primary.endpoint", :port => 1234 },
-                }],
-                :status => "available" }]
-            }
+          let(:another_redis_cache_cluster) do
+            super().merge :replication_group_id => "another-replication-group-id"
           end
 
           it "returns the most current cache cluster creation time for each replication group" do
-            client = double
-            allow(client).to receive(:describe_cache_clusters).and_return(cache_clusters)
-            allow(client).to receive(:describe_replication_groups).with(:replication_group_id => "some-group-id").and_return(replication_groups_some_cluster)
-            allow(client).to receive(:describe_replication_groups).with(:replication_group_id => "another-group-id").and_return(replication_groups_another_cluster)
-            AWS::ElastiCache::Client.stub(:new => client)
+            AWS::ElastiCache::Client.stub(:new => @client)
 
-            expect(elasticache.redis_cluster_replica_groups_for_cfn_stack).to eq [
+            actual = elasticache.redis_cluster_replica_groups_for_cfn_stack
+
+            actual.should =~ [
               {
-                "replication_group_id" => "some-group-id",
-                "primary_endpoint"     => { "address" => "some.replication.group.primary.endpoint", "port" => 1234 },
+                "replication_group_id" => "some-replication-group-id",
+                "primary_endpoint"     => { "address" => "some.replication.group.primary.endpoint", "port" => 12_345 },
                 "latest_cache_cluster_create_time" => time_earlier.strftime("%s"),
               },
               {
-                "replication_group_id" => "another-group-id",
-                "primary_endpoint"     => { "address" => "another.replication.group.primary.endpoint", "port" => 1234 },
+                "replication_group_id" => "another-replication-group-id",
+                "primary_endpoint"     => { "address" => "another.replication.group.primary.endpoint", "port" => 23_456 },
                 "latest_cache_cluster_create_time" => time_later.strftime("%s"),
               },
             ]
@@ -266,7 +287,7 @@ class Hiera
 
         it "returns all Memcached cluster nodes for CloudFormation stack of EC2 instance" do
           AWS::ElastiCache::Client.stub(:new => @client)
-          expect(elasticache.memcached_cluster_nodes_for_cfn_stack).to eq ["1.1.1.1", "2.2.2.2", "3.3.3.3", "4.4.4.4"]
+          elasticache.memcached_cluster_nodes_for_cfn_stack.should =~ ["1.1.1.1", "2.2.2.2", "3.3.3.3", "4.4.4.4"]
         end
       end
     end
